4603f2f2b0bce0ba5cc1731c8306b782f6413b97
Sangeun Han <sangeun535@gmail.com>
2020-08-13 01:51:02 +0900

    log in and log out more better

--- GURU_prj/settings.py
+++ GURU_prj/settings.py
@@ -136,3 +136,5 @@ ACCOUNT_EMAIL_REQUIRED = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/blog/'
--- GURU_prj/urls.py
+++ GURU_prj/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('accounts/', include('allauth.urls')),
+    path('', include('blog.urls')),
+
 ]
