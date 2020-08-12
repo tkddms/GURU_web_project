c511786802a2edf6e9c755989f56b59bb9715467
Sangeun Han <sangeun535@gmail.com>
2020-08-12 14:51:45 +0900

    settings 1

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+
+class PostList(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.order_by('-created')
+
+class PostDetail(DetailView):
+    model = Post
